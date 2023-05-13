8c277f731537408d26193c7a2e95d4e26cc35862
Vinícius Bohrer <vbsanttos@gmail.com>
2023-05-12 21:47:59 -0300

    Adição das classes de modelo, migrations e interfaces da camada repository

---
+++ src/main/kotlin/me/dio/credit/application/system/entity/Address.kt
@@ -0,0 +1,13 @@
+package me.dio.credit.application.system.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.Embeddable
+
+@Embeddable
+data class Address(
+    @Column(nullable = false)
+    val zipCode: String = "",
+
+    @Column(nullable = false)
+    var street: String = "",
+)
